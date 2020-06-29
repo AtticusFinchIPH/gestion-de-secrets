0a9eeac0fef230b5ef46d69b50a7dffddd09e1b1
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-06-29 23:57:16 +0200

    Add mongoose and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 
+app.use("/api/users", userRoute);
+
 app.get("/api/get", (req, res) => {
-    res.send("hello");
+    res.send("Hello, come get me!");
 });
 
 app.post("/api/secret", (req, res) => {
