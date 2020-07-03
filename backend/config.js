b8dc8fdd7c4b511201edccc6e13666e93b8e52f3
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-07-03 20:53:31 +0200

    DEPLOY Update env string

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export default {
     PORT: process.env.PORT || 5000,
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/gestion-de-secrets',
+    DATABASE_URL: process.env.MONGODB_URL || 'mongodb://localhost/gestion-de-secrets',
     JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
-}
+} 
--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import User from './models/userModel'
 
 dotenv.config();
 
-const mongodbUrl = config.MONGODB_URL;
+const mongodbUrl = config.DATABASE_URL;
 mongoose.connect(mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
