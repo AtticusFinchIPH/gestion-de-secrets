c5336ec3567aa2eedb2d9eb00df488417ceee1db
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-07-03 19:46:27 +0200

    Console create admin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -23,6 +23,7 @@ router.post("/signin", async (req, res) => {
 })
 
 router.get("/createadmin", async (req, res) => {
+    console.log("Create Admin...");
     try {
         const user = new User({
             name: 'atticus',
