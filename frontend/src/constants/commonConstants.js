afc3b736adb84315db4e611334681c47368e5e96
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2020-07-16 19:01:08 +0200

    Add CLIENT_URL to commonConstants

--- frontend/src/constants/commonConstants.js
+++ frontend/src/constants/commonConstants.js
@@ -1,5 +1,7 @@
 // dev
-// export const SERVER_URL = 'http://localhost:5000';
+export const SERVER_URL = 'http://localhost:5000';
+export const CLIENT_URL = 'http://localhost:3000';
 
 // prod
-export const SERVER_URL = 'https://secret-management.herokuapp.com';
+// export const SERVER_URL = 'https://secret-management.herokuapp.com';
+// export const CLIENT_URL = 'https://secret-management.herokuapp.com';
